70eebd091565b7b868d53ecc7ff52cc28b588459
Patryk Rozesłański <PRPaatryk9988@gmail.com>
2021-09-01 10:16:51 +0200

    Add sum of products

--- shopping/shopping/shopping.py
+++ shopping/shopping/shopping.py
@@ -6,3 +6,10 @@ print(shopping_list)
 
 for keys in shopping_list:
     print(f"Wchodzę do ", keys.capitalize() , " i kupuję tam: " , shopping_list[keys])
+
+print('\n')
+
+suma_p=(len(shopping_list["piekarnia"]))
+suma_w=(len(shopping_list["warzywniak"]))
+suma=(suma_p+suma_w)
+print(f"W sumie kupuję ",suma, " produktów")
